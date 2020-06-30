2bf573abc26e9a21ce926c9942c3f0ba0e963022
heupotato <thloanth.dut@gmail.com>
2020-06-30 14:38:27 +0700

    Update StartEvent.java
    Thay đổi tên các biến thành các tên biến có ý nghĩa
    Xoá các code dư thừa
    Thêm một số comment để làm rõ code
    Tách riêng các hàm trong event mouse_clicked thành các hàm getcurrenttime (để lấy thời gian hiện tại), readfile (đọc file) và newhistory (đưa input và output vào database)

--- src/best_travel/StartEvent.java
+++ src/best_travel/StartEvent.java
@@ -25,11 +25,12 @@ public class StartEvent implements ActionListener, MouseListener, KeyListener{
 	UIStart obj; 
 	String[] strlist; 
 	List<Integer> list = new ArrayList<Integer>(); 
-	int k, t; 
+	int numbercities, distancelimit; 
 	public StartEvent(UIStart obj) {
 		// TODO Auto-generated constructor stub
 		this.obj = obj; 
 	}
+	/**********************BUTTON EVENT*****************************/
 	/*
 	 * button browse, get directory of input file 
 	 */
@@ -53,7 +54,7 @@ public class StartEvent implements ActionListener, MouseListener, KeyListener{
 			}
 		}	
 	}
-	
+	/**********************MOUSE EVENT*****************************/
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		// TODO Auto-generated method stub
@@ -113,20 +114,23 @@ public class StartEvent implements ActionListener, MouseListener, KeyListener{
 			/*
 			 * Process and calculate the sum of distances
 			 */
-			String defaut1Text = "type something here..."; 
-			String defaultText2 = "Directory..."; 
-			if (obj.taStandard.getText().trim().equals("")||obj.taStandard.getText().trim().equals(defaut1Text)) {
-				if (obj.tfDirectory.getText().equals(defaultText2))
+			/*
+			 * check error: no input found 
+			 */
+			String taStandardOrigin = "type something here..."; 
+			String tfDirectoryOrigin = "Directory..."; 
+			if (obj.taStandard.getText().trim().equals("")||obj.taStandard.getText().trim().equals(taStandardOrigin)) {
+				if (obj.tfDirectory.getText().equals(tfDirectoryOrigin))
 				{
 					JOptionPane.showMessageDialog(null, "You have not inserted your list yet!!!\n Please insert your list.");
 					return; 
 				}
 			}
 			/*
-			 *  check if user insert the list by file or standard way 
-			 *  case1: by standard way
+			 *  check if user insert the list by file or in standard way 
+			 *  case1: in standard way
 			 */
-			if (obj.tfDirectory.getText().equals(defaultText2)) {
+			if (obj.tfDirectory.getText().equals(tfDirectoryOrigin)) {
 				try {
 					list.clear();
 					strlist = obj.taStandard.getText().trim().split("\\s+"); 
@@ -143,77 +147,57 @@ public class StartEvent implements ActionListener, MouseListener, KeyListener{
 			 * case 2: by file
 			 */
 			else {
-				try{
-					File fi = new File(obj.tfDirectory.getText()); 
-					FileReader fr = new FileReader(fi); 
-					BufferedReader br = new BufferedReader(fr);
-					
-					list.clear();
-					String str; 
-					while ((str = br.readLine())!=null) {
-						strlist = str.trim().split("\\s+"); 
-						for (int i=0; i<strlist.length; i++) {
-							list.add(Integer.parseInt(strlist[i].trim())); 
-						}
-					}
-					
-					br.close();
-					fr.close();
-						
-				}
-				catch (Exception ex) {
-					JOptionPane.showMessageDialog(null, "Invalid File Input \n Please use another file or edit your file." );
-					 return; 
-				}
-				
+				/*
+				 * use function 'readfile' to get list of distances
+				 * if the list is null, which means the file input is invalid, cancel the action
+				 */
+				list = readfile(obj.tfDirectory.getText()); 
+				if (list == null) return; 
 			}
-			
+			/*
+			 * check error: no input for number of cities or distance limitation
+			 */
 			if (obj.tfDistance.getText().trim().isEmpty()||obj.tfCityNum.getText().trim().isEmpty()) {
 				JOptionPane.showMessageDialog(null, "You have not inserted distance or/and number of cities!!!\n Please try again.");
 				return; 
 			}
+			/*
+			 * check error: input for distance limitation and number of cities is invalid
+			 */
 			try {
-				t = Integer.parseInt(obj.tfDistance.getText().trim()); 
-				k = Integer.parseInt(obj.tfCityNum.getText().trim()); 
-				if (t<0||k<0) throw new Exception(); 
+				distancelimit = Integer.parseInt(obj.tfDistance.getText().trim()); 
+				numbercities = Integer.parseInt(obj.tfCityNum.getText().trim()); 
+				if (distancelimit<0||numbercities<0) throw new Exception(); 
 			}
 			catch (Exception ex){
 				JOptionPane.showMessageDialog(null, "Invalid Input! \n Please try again\n Note: Distance Limitation and Number of Cities must be positive and integer");
 				return; 
 			}
-			if (t==0||k==0) {
+			/*
+			 * cannot find the suitable sum if distance limitation or number of cities is 0
+			 */
+			if (distancelimit==0||numbercities==0) {
 				obj.tfRes.setText("The sum does not exist!");
 				return; 
 			}
-			Integer res = Algo.chooseBestSum(t, k, list); 
+			/*
+			 * call function choosebestsum of class Algo to calculate the biggest suitable sum
+			 */
+			Integer res = Algo.chooseBestSum(distancelimit, numbercities, list); 
 			if (res == null) obj.tfRes.setText("Not exist!");
 			else obj.tfRes.setText(res + "");
 			
 			/*****************************************************/
 			/*****************************************************/
 			/*
-			 * get all of the input elements and add them to database 
-			 */
-			DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-			LocalDateTime now = LocalDateTime.now(); 
-			String time = dtf.format(now).toString(); 
-			String numlist = ""; 
-			for (int i=0; i<list.size(); i++)
-			{
-				String temp = list.get(i)+""; 
-				numlist+=temp + " ";  
-			}
-			try {
-				DataBase.addHistory(time, numlist.trim() , k, t, obj.tfRes.getText());
-			}
-			catch (Exception ex) {
-				// TODO: handle exception
-			}
-			
-		}
-		
-		
+			 * use 'newhistory' function to get all of the input elements and add them to database 
+			 */	
+			newhistory();	
+		}	
 	}
+	/*
+	 * change the appearance of buttons and labels when mouse moves over them 
+	 */
 	@Override
 	public void mouseEntered(MouseEvent e) {
 		// TODO Auto-generated method stub
@@ -232,7 +216,7 @@ public class StartEvent implements ActionListener, MouseListener, KeyListener{
 					subbtn.setBackground(new Color(100, 149, 237));
 					e.setSource(subbtn);
 				} catch (Exception e1) {
-					//JOptionPane.showMessageDialog(null, e1.toString());
+
 				}
 			}
 		}
@@ -241,6 +225,9 @@ public class StartEvent implements ActionListener, MouseListener, KeyListener{
 			
 		}
 	}
+	/*
+	 * change their appearance back when mouse exits 
+	 */
 	@Override
 	public void mouseExited(MouseEvent e) {
 		// TODO Auto-generated method stub
@@ -259,7 +246,7 @@ public class StartEvent implements ActionListener, MouseListener, KeyListener{
 					subbtn.setBackground(new Color(135, 206, 235));
 					e.setSource(subbtn);
 				} catch (Exception e1) {
-					//JOptionPane.showMessageDialog(null, e1.toString());
+					
 				}
 			}
 			
@@ -279,6 +266,11 @@ public class StartEvent implements ActionListener, MouseListener, KeyListener{
 		// TODO Auto-generated method stub
 		
 	}
+	/**********************KEY EVENT*****************************/
+	/*
+	 * if user starts typing into taStandard, 
+	 * reset it if its default text still remains 
+	 */
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
@@ -294,5 +286,66 @@ public class StartEvent implements ActionListener, MouseListener, KeyListener{
 		// TODO Auto-generated method stub
 		
 	}
+	/*
+	 *  this function get current datetime and convert it into a string
+	 */
+	private String getcurrenttime()
+	{
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+		LocalDateTime now = LocalDateTime.now(); 
+		String time = dtf.format(now).toString(); 
+		return time; 
+	}
 	
+	/*
+	 *  this function is used to get list of distances from a file
+	 *  if the input is invalid, it will return a null list
+	 */
+	private List<Integer> readfile(String url){
+		String[] strlist; 
+		List<Integer> list = new ArrayList<Integer>(); 
+		try{
+			File fi = new File(url); 
+			FileReader fr = new FileReader(fi); 
+			BufferedReader br = new BufferedReader(fr);
+			
+			
+			String str; 
+			while ((str = br.readLine())!=null) {
+				strlist = str.trim().split("\\s+"); 
+				for (int i=0; i<strlist.length; i++) {
+					list.add(Integer.parseInt(strlist[i].trim())); 
+				}
+			}
+			
+			br.close();
+			fr.close();
+				
+		}
+		catch (Exception ex) {
+			JOptionPane.showMessageDialog(null, "Invalid File Input \n Please use another file or edit your file." );
+			 return null; 
+		}
+		return list; 
+	}
+	/*
+	 * this function is used for adding the inputs and output into a database for further use
+	 */
+	public void newhistory() {
+		String time = getcurrenttime(); 
+		String numlist = ""; 
+		for (int i=0; i<list.size(); i++)
+		{
+			String temp = list.get(i)+""; 
+			numlist+=temp + " ";  
+		}
+		try {
+			DataBase.addHistory(time, numlist.trim() , numbercities, distancelimit, obj.tfRes.getText());
+		}
+		catch (Exception ex) {
+			// TODO: handle exception
+		}
+	}
+
+
 }
