aa242366c57c4f931745e1214bfadfd4b30371b6
xuanphuc97 <64199092+xuanphuc97@users.noreply.github.com>
2020-06-30 11:08:39 +0700

    update Database.java clean code
    - Thay đổi tên biến sao cho rõ nghĩa
    - Thêm một số comment giải thích

--- src/best_travel/DataBase.java
+++ src/best_travel/DataBase.java
@@ -1,23 +1,25 @@
 package best_travel;
 
 import java.sql.Statement;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class DataBase {
-	public static Connection conn;
-	static Statement sm;
+	private static Connection connection;
+	private static Statement statement;
 
+	/*
+	 * return true if database is connected
+	 */
 	public static boolean DataConnection() throws SQLException {
 		try {
 			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
 			String url = "jdbc:sqlserver://localhost:1433;integratedSecurity=true";
-			conn = DriverManager.getConnection(url);
+			connection = DriverManager.getConnection(url);
 
-			if (conn != null)
+			if (connection != null)
 				return true;
 		} catch (Exception e) {
 			return false;
@@ -25,72 +27,87 @@ public class DataBase {
 		return true;
 	}
 
+	/*
+	 * create a new database with dbo.History if it doesn't exist
+	 */
 	public static void createDataBase() throws SQLException {
 		if (DataConnection()) {
-			sm = conn.createStatement();
+			statement = connection.createStatement();
 			try {
-				sm.execute("CREATE DATABASE BestTravel");
-				sm.execute("USE BestTravel");
-				sm.execute(
-						"CREATE TABLE History(id INT IDENTITY(1,1) PRIMARY KEY , time datetime , list varchar(8000), k int, t int, output varchar(20), flag bit)");
+				statement.execute("CREATE DATABASE BestTravel");
+				statement.execute("USE BestTravel");
+				statement.execute(
+						"CREATE TABLE History(id INT IDENTITY(1,1) PRIMARY KEY , time datetime , listOfDistances varchar(8000), numberOfCities int, distanceLimitation int, output varchar(20), flag bit)");
 			} catch (Exception e) {
 			}
 		}
 	}
 
-	public static void addHistory(String time, String list, int k, int t, String ouput) throws SQLException {
+	/*
+	 * add a new record from user's using history into dbo.History
+	 */
+	public static void addHistory(String time, String listOfDistances, int numberOfCities, int distanceLimitation,
+			String ouput) throws SQLException {
 		if (DataConnection()) {
-			sm = conn.createStatement();
+			statement = connection.createStatement();
 			try {
-				sm.execute("USE BestTravel");
+				statement.execute("USE BestTravel");
 				String query = String.format(
-						"INSERT INTO History (time, list, k, t, output, flag) VALUES( '%s', '%s', %d, %d, '%s', %d)", time, list,
-						k, t, ouput, 0);
-				sm.execute(query);
+						"INSERT INTO History (time, listOfDistances, numberOfCities, distanceLimitation, output, flag) VALUES( '%s', '%s', %d, %d, '%s', %d)",
+						time, listOfDistances, numberOfCities, distanceLimitation, ouput, 0);
+				statement.execute(query);
 			} catch (Exception e) {
 				System.out.println(e.toString());
 			}
 		}
 	}
 
+	/*
+	 * delete the chosen record from dbo.History. This is not hard delete.
+	 */
 	public static void delHistory(int id) throws SQLException {
 		if (DataConnection()) {
-			sm = conn.createStatement();
+			statement = connection.createStatement();
 			try {
-				sm.execute("USE BestTravel");
+				statement.execute("USE BestTravel");
 				String query = String.format("UPDATE History SET flag = 1 WHERE id = %d", id);
-				sm.execute(query);
+				statement.execute(query);
 			} catch (Exception e) {
 			}
 		}
 	}
 
+	/*
+	 * delete the all record from dbo.History. This is not hard delete.
+	 */
 	public static void delAllHistory() throws SQLException {
 		if (DataConnection()) {
-			sm = conn.createStatement();
+			statement = connection.createStatement();
 			try {
-				sm.execute("USE BestTravel");
-				sm.execute("UPDATE History SET flag = 1 WHERE flag = 0");
+				statement.execute("USE BestTravel");
+				statement.execute("UPDATE History SET flag = 1 WHERE flag = 0");
 			} catch (Exception e) {
 			}
 		}
 	}
 
+	/*
+	 * get ResultSet from dbo.History to show in JTable
+	 */
 	public static ResultSet getAllHistory() throws SQLException {
-		ResultSet rs = null;
+		ResultSet historyResultSet = null;
 		if (DataConnection()) {
-			sm = conn.createStatement();
+			statement = connection.createStatement();
 			try {
-				sm.execute("USE BestTravel");
-				rs = sm.executeQuery("SELECT * FROM History WHERE flag = 0");
+				statement.execute("USE BestTravel");
+				historyResultSet = statement.executeQuery("SELECT * FROM History WHERE flag = 0");
 			} catch (Exception e) {
 			}
 		}
 
-		return rs;
+		return historyResultSet;
 	}
-	
-	public static void main(String[] args) throws SQLException {
 
+	public static void main(String[] args) throws SQLException {
 	}
 }
